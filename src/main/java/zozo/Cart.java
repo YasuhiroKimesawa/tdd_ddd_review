98454aef22aa1afbcccb7286d2b8a4d7384499a3
naoyasugita <sugita6878@gmail.com>
2022-02-09 13:46:47 +0900

    WIP: addGoodsの仮実装

--- src/main/java/zozo/Cart.java
+++ src/main/java/zozo/Cart.java
@@ -1,20 +1,48 @@
 package zozo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cart {
+
+  private final List<Good> goods;
+
   public Cart() {
+    goods = List.of();
   }
 
   @Override
-  public boolean equals(Object obj) {
-    return true;
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Cart cart = (Cart) o;
+    return goods.equals(cart.goods);
+  }
+
+  @Override
+  public String toString() {
+    return "Cart{" +
+        "goods=" + goods +
+        '}';
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(goods);
   }
 
   public Cart(List<Good> goods) {
+    this.goods = goods;
   }
 
   public Cart addGoods(Good good, int i) {
-    return new Cart();
+    var list = new ArrayList<>(goods);
+    list.add(good);
+    return new Cart(list);
   }
 }
