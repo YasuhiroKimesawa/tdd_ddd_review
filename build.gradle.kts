bd6ac4c99f01228156476288b741a7a78f04a8f2
Junichi Kato <j5ik2o@gmail.com>
2022-08-24 16:57:32 +0900

    Spring bootの設定を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,19 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.0.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
     id("com.diffplug.spotless") version "6.3.0"
     id("org.flywaydb.flyway") version "8.5.13"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 
 spotless {
     java {
@@ -24,6 +32,8 @@ build.dependsOn(spotlessApply)
 
 repositories {
     mavenCentral()
+    maven("https://repo.spring.io/milestone")
+    maven("https://repo.spring.io/snapshot")
 }
 
 dependencies {
@@ -33,6 +43,11 @@ dependencies {
     implementation("io.vavr:vavr:0.10.4")
     implementation("org.mybatis:mybatis:3.5.10")
     implementation("mysql:mysql-connector-java:8.0.30")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.flywaydb:flyway-core:8.5.13")
     testImplementation("org.flywaydb:flyway-mysql:8.5.13")
     testImplementation("org.testcontainers:junit-jupiter:1.17.3")
