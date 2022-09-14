1757449225b732211a42358c01197cb499e7d973
Junichi Kato <j5ik2o@gmail.com>
2022-09-14 14:37:22 +0900

    mybatis-spring-boot-starterを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,13 +39,14 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("com.h2database:h2:2.1.214")
     implementation("io.vavr:vavr:0.10.4")
-    implementation("org.mybatis:mybatis:3.5.10")
+//    testImplementation("com.h2database:h2:2.1.214")
+   // implementation("org.mybatis:mybatis:3.5.10")
     implementation("mysql:mysql-connector-java:8.0.30")
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.flywaydb:flyway-core:8.5.13")
